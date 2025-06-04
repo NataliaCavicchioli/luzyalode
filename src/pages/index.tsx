922184d45530959e9ae870a285a450dc7c120114
Natalia Cavicchioli <na.cavicchioli@gmail.com>
2025-06-04 09:58:51 -0300

    refactor: remove html index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,14 @@ export default function Home() {
       <Head>
         <title>Luzyalode Terapias</title>
         <link rel="icon" href="/spa.svg" type="image/svg+xml" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Banner />
       <ProductGrid />
