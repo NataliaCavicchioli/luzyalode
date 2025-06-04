1eb51b35151ee7b535797c968ac498e4fd5bf29d
Natalia Cavicchioli <na.cavicchioli@gmail.com>
2025-06-04 09:52:19 -0300

    feat: create document to add gtm in body

---
+++ src/pages/_document.tsx
@@ -0,0 +1,34 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+        <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-MPS8STZS"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
+
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
